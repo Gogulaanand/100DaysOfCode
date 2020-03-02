7718c41851e8fec62b6f9154e4a6c3c4c641b0b6
Gogulaanand R <gogulaanand02@gmail.com>
2020-03-02 18:54:49 +0530

    smallerNumbersThanCurrent

--- LeetCode problems/ALL_PROBLEMS.py
+++ LeetCode problems/ALL_PROBLEMS.py
@@ -110,5 +110,34 @@ assert twoSum([10,8,12,8],16)
 
 
 
+# https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+# How Many Numbers Are Smaller Than the Current Number
+
+# Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+# Return the answer in an array.
+
+# Example 1:
+
+# Input: nums = [8,1,2,2,3]
+# Output: [4,0,1,1,3]
+
+# Sol 1: (Brute-force)
+
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        out = []
+        for i in range(len(nums)):
+            l = nums[:]
+            l.pop(i)
+            current = nums[i]
+            count = 0 
+            for item in l:
+                if item<current and item!=current:
+                    count+=1
+            out.append(count)
+        return out
+
+
 
 
