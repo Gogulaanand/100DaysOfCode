78ee5cacc2676feacbbf5e25af403fbcd9fd55d2
Gogulaanand R <gogulaanand02@gmail.com>
2020-03-19 19:05:02 +0530

    univalued binary tree

---
+++ LeetCode problems/UnivaluedBinaryTree.py
@@ -0,0 +1,35 @@
+# Univalued Binary Tree
+
+# A binary tree is univalued if every node in the tree has the same value.
+
+# Return true if and only if the given tree is univalued.
+
+
+#Sol 1: iterative BFS
+class Solution:
+    def isUnivalTree(self, root: TreeNode) -> bool:
+        l = [root.val]
+        queue = [root]
+        while(queue):
+            current = queue.pop(0)
+            if current.val!=l[-1]:
+                return False
+            else:
+                l.append(current.val)
+            if current.left:
+                queue.append(current.left)
+            if current.right:
+                queue.append(current.right)
+        l.pop(0)
+        if len(l) == l.count(l[0]):
+            return True
+        else:
+            return False
+
+#Sol 2: recursive DFS
+
+class Solution:
+    def isUnivalTree(self, root: TreeNode) -> bool:
+        def dfs(node: TreeNode) -> bool:
+            return node is None or node.val == root.val and dfs(node.left) and dfs(node.right)
+        return dfs(root)
